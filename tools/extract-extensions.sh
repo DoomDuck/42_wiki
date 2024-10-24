b1dc64b98635983649c14a1e1c8959b878c2cb83
Brieuc Poumeau De Lafforest <delafforest.brieuc@gmail.com>
2024-10-24 14:44:35 +0200

    Debug: suppress debugs echo

--- tools/extract-extensions.sh
+++ tools/extract-extensions.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-set -o
-
-echo extracting extensions
-
 archives=$(find extensions -name "*.tgz");
 
 for archive in $archives; do
-    echo "$archive";
     tar -C "./extensions" -xzf "$archive";
 done
 
